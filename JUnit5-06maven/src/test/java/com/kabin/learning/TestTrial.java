2f5c19e2b158c341316088128e0f1dc40c18a32a
Kabin_giri <85396529+Kabin119245@users.noreply.github.com>
2024-04-23 14:26:06 +0545

    Update TestTrial.java
    added implementation of Supplier Interface using lambda expression

--- JUnit5-06maven/src/test/java/com/kabin/learning/TestTrial.java
+++ JUnit5-06maven/src/test/java/com/kabin/learning/TestTrial.java
@@ -8,15 +8,19 @@ class TestTrial {
 	
 	Shapes sh = new Shapes();
 
-	@Test
-	void test() {
-		assertEquals(6,3+3);
-	}
+	
 	@Test
 	void testcomputeSquareArea() {
-		assertEquals(36, sh.computeSquareArea(6));
+		assertEquals(36, sh.computeSquareArea(6),"Calculation of area is wrong");
 		
+		//we can do by implementation of Supplier interface where message will not be evaluated if the test is passes
 		
+	}
+	
+	@Test
+	void testcomputeCircleArea_Supplier() {
+		assertEquals(113.04, sh.computeCircleArea(6), () -> "Message to dev");
+		//we can also use assertNotEquals which gives just opposite result of assertEquals
 	}
 	
 	
