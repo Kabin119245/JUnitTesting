1224a923927be3b368dada83286177546265b31a
Kabin_giri <85396529+Kabin119245@users.noreply.github.com>
2024-04-24 00:25:38 +0545

    Update ShapesTest.java
    annotation added, behaviour changed, beforeall, afterall, beforeeach, aftereach, testInstance

--- Junit-05/test/com/kabin/learning/ShapesTest.java
+++ Junit-05/test/com/kabin/learning/ShapesTest.java
@@ -2,22 +2,60 @@ package com.kabin.learning;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+//if we want to create only one instance then we use @TestInstance annotation
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ShapesTest {
+	 ShapesTest() {
+		System.out.println("Test obj is created");
+	}
+	 //we have changed the behaviour to PER_CLASS to create only one object so no need to make static method, previously static method were used 
+	 //if PER.MEthod -> before all and after all method should be static
+	@BeforeAll
+	 void beforeAll() {
+		System.out.println("Before all tests");
+		
+	}
 	
-	Shapes sh = new Shapes();
+	Shapes shape;
+	@BeforeEach //before each test it will be executed
+	void init() {
+		
+		shape = new Shapes();
+		System.out.println("Before each test");
+		
+	}
+	
 
 	@Test
 	void testcomputeSquareArea() {
 		
-		assertEquals(36, sh.computeSquareArea(6));
+		assertEquals(36, shape.computeSquareArea(6));
+		System.out.println("Actual Test Running");
 	}
 	
 	@Test
 	void testcomputeCircleArea() {
 		
-		assertEquals(113.04, sh.computeCircleArea(6));
+		assertEquals(113.04, shape.computeCircleArea(6));
+		System.out.println("Actual Test Running");
+	}
+	
+	@AfterEach
+	void destroy() {
+		//closing resources
+		System.out.println("After each test");
+	}
+	
+	@AfterAll
+	 void afterAll() {
+		System.out.println("After all tests");
 	}
 
 }
